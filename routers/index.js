8d3904f6019cd9da56c980fd9dbf96df5c514cf8
Dhwaj Sharma <dhwajsharma@Dhwajs-MacBook-Pro.local>
2023-07-11 21:38:22 -0700

    Fixed Spelling mistake bug

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const questionRouter = require("./Question");
-const answerRouter = require("./Asnwer");
-const commentRouter = require('./Comments')
+const answerRouter = require("./Answer");
+const commentRouter = require("./Comments");
 
 router.get("/", (req, res) => {
   res.send("Welcome to stack overflow clone");
@@ -11,6 +11,6 @@ router.get("/", (req, res) => {
 
 router.use("/question", questionRouter);
 router.use("/answer", answerRouter);
-router.use('/comment', commentRouter)
+router.use("/comment", commentRouter);
 
 module.exports = router;
